258b93e7dcbabe49908f4ac9cb1c465fcbb2cf67
mamorshedalam <ma.morshedalampro@gmail.com>
2023-08-03 09:09:31 +0800

    project init

---
+++ src/layouts/rootLayout.jsx
@@ -0,0 +1,24 @@
+
+
+
+export default function RootLayout() {
+
+
+     return (
+          <>
+               <header className="flex flex-wrap justify-between items-center px-12 py-4">
+                    <a href="" className="uppercase font-bold tracking-wider text-2xl"><span className="font-black tracking-widest text-orange-400">#</span> morshed alam</a>
+                    <nav className="font-semibold text-lg">
+                         <ul className="sl-nav flex item-center gap-9">
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>home</a></li>
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>about</a></li>
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>expertise</a></li>
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>work</a></li>
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>experience</a></li>
+                              <li><a href=""><span className="font-black tracking-widest text-orange-400">//</span>contact</a></li>
+                         </ul>
+                    </nav>
+               </header>
+          </>
+     )
+} 
---
+++ src/routes/root.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/rootLayout";
+import Home from "../pages/home";
+import Contact from "../pages/contact";
+import Project from "../pages/project";
+
+
+const routes = createBrowserRouter([
+     {
+          path: '/',
+          element: <RootLayout />,
+          children: [
+               { index: true, element: <Home /> },
+               {
+                    path: '/project',
+                    element: <Project />
+               },
+               {
+                    path: '/contact',
+                    element: <Contact />
+               }
+          ]
+     }
+])
+
+export default routes;
