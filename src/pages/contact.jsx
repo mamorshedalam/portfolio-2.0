ab66a102d19011383bfd639cda5e84a86cdc7efd
mamorshedalam <ma.morshedalampro@gmail.com>
2023-08-07 19:13:55 +0800

    contact page completed

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,21 +5,24 @@ export default function Contact() {
 
 
      return (
-          <section className="md:py-20 xs:py-16 py-12">
-               <h2 className="text-center font-bold text-4xl">Let's Cook Something...</h2>
-               <div className="w-1/2 mx-auto">
+          <section className="md:pt-28 xs:pt-20 py-14 md:px-6 sm:px-4 px-2">
+               <h2 className="text-center font-bold md:text-4xl sm:text-3xl text-2xl mb-9">Let's Cook Something Together <span className="text-orange-400">.</span></h2>
+               <div className="lg:w-1/2 md:w-3/4 w-full mx-auto">
                     <form action="" className="flex flex-wrap">
-                         <div className="w-1/2 pr-3">
+                         <div className="sm:w-1/2 w-full sm:pr-3 xl:mb-6 mb-4">
                               <label htmlFor="name">Name</label>
-                              <input id="name" type="text" className="bg-neutral-800 rounded px-6 py-4 mt-2 border border-neutral-700" />
+                              <input id="name" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" />
                          </div>
-                         <div className="w-1/2 pl-3">
+                         <div className="sm:w-1/2 w-full sm:pl-3 xl:mb-6 mb-4">
                               <label htmlFor="mail">Email</label>
-                              <input id="mail" type="text" className="bg-neutral-800 rounded px-6 py-4 mt-2 border border-neutral-700" />
+                              <input id="mail" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" />
                          </div>
-                         <div className="w-full mt-6">
+                         <div className="w-full xl:mb-6 mb-4">
                               <label htmlFor="message">Message</label>
-                              <textarea id="message" name="" className="bg-neutral-800 rounded px-6 py-4 h-40 mt-2 border border-neutral-700"></textarea>
+                              <textarea id="message" name="" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 md:h-40 h-32 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated"></textarea>
+                         </div>
+                         <div className="w-full text-right">
+                              <button className="xl:px-6 px-4 xl:py-3 py-2 rounded border-2 border-sky-50 hover:text-orange-400 hover:border-orange-400 sl-animated-xl">Send Message</button>
                          </div>
                     </form>
                </div>
