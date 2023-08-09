61653288a403ffc70d762a898d5aad5b588f8c93
mamorshedalam <ma.morshedalampro@gmail.com>
2023-08-09 19:03:18 +0800

    form requred added

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -29,15 +29,15 @@ export default function Contact() {
                     <form ref={form} onSubmit={sendEmail} className="flex flex-wrap">
                          <div className="sm:w-1/2 w-full sm:pr-3 xl:mb-6 mb-4">
                               <label htmlFor="name">Name</label>
-                              <input name="sender_name" id="name" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" />
+                              <input name="sender_name" id="name" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" required />
                          </div>
                          <div className="sm:w-1/2 w-full sm:pl-3 xl:mb-6 mb-4">
                               <label htmlFor="mail">Email</label>
-                              <input name="sender_email" id="mail" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" />
+                              <input name="sender_email" id="mail" type="text" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" required />
                          </div>
                          <div className="w-full xl:mb-6 mb-4">
                               <label htmlFor="message">Message</label>
-                              <textarea name="sender_message" id="message" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 md:h-40 h-32 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated"></textarea>
+                              <textarea name="sender_message" id="message" className="bg-neutral-800 rounded xl:px-6 md:px-4 px-2 xl:py-4 md:py-2 py-1.5 md:h-40 h-32 mt-2 border border-neutral-700 hover:border-orange-400 focus:border-orange-400 sl-animated" required></textarea>
                          </div>
                          <div className="w-full text-right">
                               <button className="xl:px-6 px-4 xl:py-3 py-2 rounded border-2 border-sky-50 hover:text-orange-400 hover:border-orange-400 sl-animated-xl">Send Message</button>
